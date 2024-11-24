720826163c6e2eabc013d9f98154dcb3ed99bbec
bimaryan <bimaryan046@gmail.com>
2024-11-24 13:39:55 +0700

    "Updated App.css with new font import and styles, refactored Navbar.js with new layout and icons, and modified Home/Index.js with new sections and content"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,25 +23,23 @@ function Navbar() {
     };
 
     return (
-        <nav className="z-10 sticky top-0 w-full bg-gray-200 dark:bg-gray-900 shadow">
-            <div className="container mx-auto">
-                <div className="flex items-center justify-between max-w-screen-xl mx-auto p-4 rounded-full">
-                    <NavLink to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <span className="self-center text-1xl whitespace-nowrap text-gray-900 dark:text-white title">
-                            Ryaze.my.id
-                        </span>
-                    </NavLink>
-                    <button
-                        onClick={toggleDarkMode}
-                        className="px-2 py-1 bg-gray-900 rounded-full dark:bg-white text-white dark:text-gray-900"
-                    >
-                        {isDarkMode ? (
-                            <i className="bi bi-sun"></i>
-                        ) : (
-                            <i className="bi bi-moon-stars"></i>
-                        )}
-                    </button>
-                </div>
+        <nav className="z-10 sticky top-0 w-full max-w-screen mx-auto p-4 bg-gray-100 dark:bg-gray-900 shadow-xl">
+            <div className="flex flex-wrap items-center justify-between max-w-xl mx-auto">
+                <NavLink to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <span className="self-center font-bold text-1xl whitespace-nowrap text-gray-900 dark:text-white title">
+                        <i class="fa-solid fa-code bg-gray-900 dark:bg-white rounded p-1 text-white dark:text-gray-900"></i> ryaze
+                    </span>
+                </NavLink>
+                <button
+                    onClick={toggleDarkMode}
+                    className="px-2 py-1 bg-gray-900 rounded-full dark:bg-white text-white dark:text-gray-900"
+                >
+                    {isDarkMode ? (
+                        <i className="bi bi-sun"></i>
+                    ) : (
+                        <i className="bi bi-moon-stars"></i>
+                    )}
+                </button>
             </div>
         </nav>
     );
--- src/components/home/Index.js
+++ src/components/home/Index.js
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <div className="max-w-screen-xl mx-auto p-4">
-            <div className="space-y-6">
-                <h1 className="text-gray-900 dark:text-white text-2xl">Hi, I'm Bima Ryan Alfarizi 👋</h1>
-                <p className="ext-gray-900 dark:text-white font-normal text-m">I'm junior software engineer from indonesia.</p>
+        <main className="max-w-xl mx-auto p-5 mt-4">
+            <section className="space-y-6">
+                <h1 className="text-gray-900 dark:text-white font-semibold text-4xl">Hi, I'm Bima Ryan Alfarizi 👋</h1>
+                <p className="ext-gray-900 dark:text-white font-normal text-lg">I'm junior software engineer from indonesia.</p>
+                <p className="ext-gray-900 dark:text-white font-normal text-lg">Lost in melodies 🎵 with a cup of coffee ☕ and a relaxing cigarette 🚬 at a cozy café is my routine activity.</p>
                 <div className="space-y-6">
-                    <p className="text-m text-gray-900 dark:text-white font-normal">Find me on</p>
+                    <p className="text-lg text-gray-900 dark:text-white font-normal">Find me on</p>
                     <div className="flex gap-2">
                         <div>
                             <a href="https://facebook.com/bima_ryan23" target="_blank" className="bg-gray-200 dark:bg-gray-900 text-gray-900 dark:text-white rounded px-4 py-2 text-lg"><i class="fa-brands fa-facebook"></i></a>
@@ -21,47 +22,70 @@ function Home() {
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
 
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-3 mt-10">
-                <div className="w-full p-3 bg-gray-200 dark:bg-gray-900 text-gray-900 dark:text-white shadow rounded">
-                    <figure className="max-w-lg">
-                        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.com/docs/images/examples/image-3@2x.jpg" alt="image description" />
-                        <figcaption className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Project</figcaption>
-                    </figure>
-                    <div className="flex gap-3">
-                        <Link to="/project" className="w-full"><i className="bi bi-code-slash"></i> Project</Link>
-                        <button className="ml-auto">
-                            <i className="bi bi-three-dots-vertical"></i>
-                        </button>
+            <section className="space-y-6 mt-14">
+                <h1 className="font-semibold text-3xl md:text-4xl text-gray-900 dark:text-white">Skill</h1>
+                <div className="grid grid-cols-5 md:grid-cols-6 gap-5">
+                    <a>
+                        <img class="w-16" alt="javascript" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"></img>
+                    </a>
+                    <a>
+                        <img class="w-16" alt="nodejs" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-plain.svg" />
+                    </a>
+                    <a>
+                        <img class="w-16" alt="react" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"></img>
+                    </a>
+                    <a>
+                        <img class="w-16" alt="tailwindcss" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
+                    </a>
+                    <a>
+                        <img class="w-16" alt="bootstrap" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg"></img>
+                    </a>
+                    <a>
+                        <img class="w-16" alt="html" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
+                    </a>
+                    <a>
+                        <img class="w-16" alt="css" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />
+                    </a>
+                </div>
+            </section>
+
+            {/* <section className="space-y-6 mt-14">
+                <h1 className="font-semibold text-3xl md:text-4xl text-gray-900 dark:text-white">Blog</h1>
+            </section> */}
+
+            <section className="space-y-6 mt-14">
+                <h1 className="font-semibold text-3xl md:text-4xl text-gray-900 dark:text-white">Work</h1>
+                <div className="bg-gray-100 w-full dark:bg-gray-900 border border-gray-900 dark:border-white rounded-lg p-6 w-full shadow-lg">
+                    <Link to="/project" className="flex justify-between cursor-pointer">
+                        <div className="flex gap-2 items-center">
+                            <img class="w-5 h-5" alt="polindra" src="image/polindra.png" />
+                            <p className="font-bold text-lg text-gray-900 dark:text-white">SILK</p>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-arrow-up-right-from-square text-gray-900 dark:text-white"></i>
+                        </div>
+                    </Link>
+                    <hr className="my-3 bg-gray-200 dark:bg-gray-900" />
+                    <p className="text-sm text-gray-900 dark:text-white">Aplikasi peminajaman bahan dan alat laboratorium adalah sebuah sistem berbasis teknologi yang dirancang untuk mempermudah proses peminjaman, pengembalian, serta pengelolaan inventaris laboratorium secara digital.</p>
+                    <div className="flex justify-end items-center gap-3 mt-4">
+                        <div>
+                            <i class="fa-brands fa-laravel text-gray-900 dark:text-white"></i>
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-database text-gray-900 dark:text-white"></i>
+                        </div>
+                        <div>
+                            <img class="w-5" alt="tailwindcss" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" />
+                        </div>
+                        <div>
+                            <i class="fa-brands fa-js text-gray-900 dark:text-white"></i>
+                        </div>
                     </div>
                 </div>
-                <div className="w-full p-3 bg-gray-200 dark:bg-gray-900 shadow text-gray-900 dark:text-white rounded">
-                    <figure className="max-w-lg">
-                        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.com/docs/images/examples/image-3@2x.jpg" alt="image description" />
-                        <figcaption className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">About</figcaption>
-                    </figure>
-                    <div className="flex gap-3">
-                        <Link to="/project" className="w-full"><i className="bi bi-info-circle"></i> About</Link>
-                        <button className="ml-auto">
-                            <i className="bi bi-three-dots-vertical"></i>
-                        </button>
-                    </div>
-                </div>
-                <div className="w-full p-3 bg-gray-200 dark:bg-gray-900 shadow text-gray-900 dark:text-white rounded">
-                    <figure className="max-w-lg">
-                        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.com/docs/images/examples/image-3@2x.jpg" alt="image description" />
-                        <figcaption className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Contact</figcaption>
-                    </figure>
-                    <div className="flex gap-3">
-                        <Link to="/project" className="w-full"><i className="bi bi-telephone-inbound"></i> Contact</Link>
-                        <button className="ml-auto">
-                            <i className="bi bi-three-dots-vertical"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+            </section>
+        </main>
     );
 }
 
